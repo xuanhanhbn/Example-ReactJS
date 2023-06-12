72d6f7a4c8b61adfb6dbd5f1931f221be45dbda2
duong-s-97 <duongdev97@gmail.com>
2023-06-12 11:12:50 +0700

    get value and validation tabSecurity

---
+++ src/views/account-settings/constants.js
@@ -0,0 +1,17 @@
+export const inputTabSecurity = [
+    {
+        field: 'currentPassword',
+        lable: 'Current Password',
+        type: 'password'
+    },
+    {
+        field: 'newPassword',
+        lable: 'New Password',
+        type: 'password'
+    },
+    {
+        field: 'confirmNewPassword',
+        lable: 'Confirm New Password',
+        type: 'password'
+    }
+]
