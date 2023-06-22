5bdcb9e8f37030bbd2aa5c35a0d9697f6c149d43
Nguyenthai27 <ptejthaii@gmail.com>
2023-06-22 20:52:27 +0700

    ver 2

--- src/pages/staff/index.js
+++ src/pages/staff/index.js
@@ -1,5 +1,6 @@
 const { Decagram } = require("mdi-material-ui");
 
+import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 
@@ -8,10 +9,13 @@ import TableCollapsible from 'src/views/staff'
 
 const MUITable = () => {
     return (
-        <Card>
-            <CardHeader title='Quản lí hoạt động nhân sự' titleTypographyProps={{ variant: 'h6' }} />
-            <TableCollapsible />
-        </Card>
+        <Grid item xs={12}>
+            <Card>
+                <CardHeader title='Quản lí hoạt động nhân sự' titleTypographyProps={{ variant: 'h6' }} />
+                <TableCollapsible />
+            </Card>
+        </Grid>
+
     )
 }
 
