a8b016c2afe08f6a1e292c7f2a8d9cc1c5f7f08d
duong-s-97 <duongdev97@gmail.com>
2023-07-03 16:44:20 +0700

    ui list docs

--- src/views/admin/documents/constants.js
+++ src/views/admin/documents/constants.js
@@ -1,30 +1,30 @@
 export const inputSearchDocument = [
   {
-    field: 'name',
-    lable: 'Name'
+    field: 'title',
+    lable: 'Title'
   },
   {
-    field: 'creator',
-    lable: 'Creator'
+    field: 'author',
+    lable: 'Author'
   }
 ]
 
-function createData(name, creator, creationDate) {
-  return { name, creator, creationDate }
-}
+// function createData(name, creator, creationDate) {
+//   return { name, creator, creationDate }
+// }
 
-export const columns = [
-  { id: 'name', label: 'Name', minWidth: 300 },
-  { id: 'creator', label: 'Creator', minWidth: 170, align: 'right' },
-  { id: 'creationDate', label: 'Creation Date', minWidth: 170, align: 'right' }
-]
+// export const columns = [
+//   { id: 'name', label: 'Name', minWidth: 300 },
+//   { id: 'creator', label: 'Creator', minWidth: 170, align: 'right' },
+//   { id: 'creationDate', label: 'Creation Date', minWidth: 170, align: 'right' }
+// ]
 
-export const rows = [
-  createData('qưe', 'Admin', '23-6-2023'),
-  createData('rty', 'Admin', '24-6-2023'),
-  createData('uio', 'Admin', '25-6-2023'),
-  createData('pas', 'Admin', '26-6-2023'),
-  createData('dfg', 'Admin', '27-6-2023'),
-  createData('hjk', 'Admin', '28-6-2023'),
-  createData('lzx', 'Admin', '29-6-2023')
-]
+// export const rows = [
+//   createData({ title: 'ddd' }, 'Admin', '23-6-2023'),
+//   createData('rty', 'Admin', '24-6-2023'),
+//   createData('uio', 'Admin', '25-6-2023'),
+//   createData('pas', 'Admin', '26-6-2023'),
+//   createData('dfg', 'Admin', '27-6-2023'),
+//   createData('hjk', 'Admin', '28-6-2023'),
+//   createData('lzx', 'Admin', '29-6-2023')
+// ]
